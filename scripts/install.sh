5a66aa6e38fa244d141712ef476c8732a980457d
o-xian <o-xian@onoyama1999910@gmail.com>
2020-04-06 23:34:17 +0900

    Make scripts directory

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt-get install vim-gnome -y
+sudo apt-get instsll git -y
+sudo apt-get install g++ -y
+sudo apt-get install curl -y
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
