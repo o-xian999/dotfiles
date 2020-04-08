ed87a1ae1e2871f5841c1c850c22b75c407acf62
o-xian <onoyama1999910@gmail.com>
2020-04-08 16:46:35 +0900

    Add tree command

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sudo apt-get install g++ -y
 sudo apt-get install curl -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+sudo apt-get install tree -y
