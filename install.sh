613109e1561c372814b9e18c1d8274bbfbf21c0a
o-xian <onoyama1999910@gmail.com>
2020-04-11 14:02:46 +0900

    Move sh files to scripts directory

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt-get install vim-gnome -y
-sudo apt-get instsll git -y
-sudo apt-get install g++ -y
-sudo apt-get install curl -y
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo apt-get install tree -y
