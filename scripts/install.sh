5a4863e694a415fb00617849a71ecd32ce5f4ae6
o-xian <o-xian@onoyama1999910@gmail.com>
2020-04-18 22:45:15 +0900

    Edit many

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,3 +6,8 @@ sudo apt-get install curl -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt-get install tree -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
+sudo apt install ./vscode.deb
