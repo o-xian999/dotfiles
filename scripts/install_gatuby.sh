9331fdd0159ee0b809d7ed520ed3f66cf0ba8ae6
o-xian <onoyama1999910@gmail.com>
2020-09-07 14:57:13 +0900

    Edit gitconfig

--- scripts/install_gatuby.sh
+++
@@ -1,6 +0,0 @@
-#!bin/sh
-sudo apt-get install build-essential -y
-sudo apt-get install libssl-dev -y
-git clone git://github.com/creationix/nvm.git ~/.nvm
-source ~/.nvm/nvm.sh
-npm install -g gatsby-cli
