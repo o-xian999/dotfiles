96398c708151db4d42a431e6492018d1489520e0
o-xian999 <onoyama1999910@gmail.com>
2020-09-13 00:01:08 +0900

    Add install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!bin/sh
+  sudo apt install -y vim
+  sudo apt remove -y firefox
+  sudo apt install -y git 
+  sudo apt install -y terminator
+  sudo apt install -y neofetcht
+  sudo apt install -y curl
+  sudo apt install -y python3-pip
+  sudo pip3 install -y powerline-shell
+  sudo apt-get install -y fonts-powerline
+
